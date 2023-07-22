11207ab67c66854a787bd1691826f397ebe81a64
momokapool <lgbao2004@gmail.com>
2023-07-22 21:58:47 +0700

    Update kcj.cpp

--- program year 1 ae/C-C++ project hay dai loai the/kcj.cpp
+++ program year 1 ae/C-C++ project hay dai loai the/kcj.cpp
@@ -2,7 +2,7 @@
 int main(){
 	int age = 24;
 	char name[]="Suzuhara Asahi";
-	char description[]="Software developer, Tokyo";
+	char description[]="Software Engineer, Tokyo";
 	float salary_per_month= 3500.5;
 	printf("Boku wa %s desu\n", name);
 	printf("%dsai desu\n", age);
